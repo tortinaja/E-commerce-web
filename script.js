dfd08c1f92dbf1ce9157fddb64f73c900e06d684
tortinaja <79805888+tortinaja@users.noreply.github.com>
2021-03-28 00:02:12 +0700

    respon2

---
+++ script.js
@@ -0,0 +1,16 @@
+const bgblock1 = document.getElementById('bgblock1');
+const txtblock2 = document.getElementById('txtblock2');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+
+btn1.addEventListener('click', function(){
+    bgblock1.style.backgroundColor = "blue";
+});
+
+btn2.addEventListener('click', function(){
+    txtblock2.style.color = "red";
+});
+
+if (btn1.addEventListener('click', function(){
+    bgblock1.style.backgroundColor = "blue";
+    }));
